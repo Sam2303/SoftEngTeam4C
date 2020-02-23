918b4464b9f84ad57bd025704fa16cee46568ab4
Harry Martin <up896106@myport.ac.uk>
2020-02-23 19:06:03 +0000

    initial database plan

--- src/setup.sql
+++ src/setup.sql
@@ -16,3 +16,12 @@ create table test(
 insert into test(test_string, test_integer) values
 ('Hello', 123),
 ('World', 456);
+
+-- create user table  (email, password, (PK - userID))
+-- create thread table (question-text, date, (PK - threadID))
+
+-- create user-thread (userID, threadID, (PK - thread-userID))  handles relationship between user and their threads.
+
+-- create response table (response-text, up/down-score, (PK - responseID))
+
+-- create user-response table (userID, responseID, (PK - user-responseID)) handles relationship between user and their responses
