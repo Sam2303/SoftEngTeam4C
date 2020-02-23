9a5ca8fe94161bb05095f9ca5c7fa1fe6c7d2d5d
thatguywiththatname <simjenner3@gmail.com>
2020-02-23 01:45:56 +0000

    Basic api.js, small changes to server and db, update readme

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const db = require("./database");
 const express = require("express");
 const path = require("path");
+const api = require("./api");
+const db = require("./database");
 
 // TODO: Use JSDoc -> https://jsdoc.app/
 
@@ -8,21 +9,15 @@ const app = express();
 
 app.use(express.json());
 app.use("/static", express.static(__dirname + "/static"));
+app.use("/api", api);
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
-app.listen(8080);
-
-/** TEMPORARY FUNCTION - testing database interface */
-async function test()   {
-    db.connect();
-    const data = await db.query(`select * from test;`);
-    for(let x = 0; x < data.rowCount; x++){
-        console.log(data.rows[x]['test_pk'] + " | " + data.rows[x]['test_string'] + " | " + data.rows[x]['test_integer']);
-    }
-    db.disconnect();
-}
-test();
-
+// Async stuff can't be run at the top level so this fuckery has to happen
+// https://stackoverflow.com/a/46515787
+(async () => {
+    await db.connect();
+    app.listen(8080);
+})();
