e66af53e48c41f3f2352edf07c80d6a5444f3464
Harry Martin <up896106@myport.ac.uk>
2020-02-25 14:59:26 +0000

    added initial database design with basic test case

--- src/setup.sql
+++ src/setup.sql
@@ -17,11 +17,41 @@ insert into test(test_string, test_integer) values
 ('Hello', 123),
 ('World', 456);
 
--- create user table  (email, password, (PK - userID))
--- create thread table (question-text, date, (PK - threadID))
+create table fpp_user(
+    id          serial primary key,
+    email       varchar(50) not null,
+    password    varchar(50) not null
+);
 
--- create user-thread (userID, threadID, (PK - thread-userID))  handles relationship between user and their threads.
+create table thread(
+    id          serial primary key,
+    text        text not null,
+    date        date not null,
+    user_id     integer         references fpp_user(id)
+);
 
--- create response table (response-text, up/down-score, (PK - responseID))
+create table response(
+    id          serial primary key,
+    text        text not null,
+    score       integer         default 0,
+    user_id     integer         references fpp_user(id),
+    thread_id   integer         references thread(id)
+);
 
--- create user-response table (userID, responseID, (PK - user-responseID)) handles relationship between user and their responses
+-- insert test user
+insert into fpp_user(email, password) values
+('test-email@test.ac.uk', 'ekfrowgnrjwefr');
+
+-- insert test thread
+insert into thread(text, date, user_id) values
+('I need air! I can''t breath!', now(), 1);
+
+-- insert test response to thread 
+insert into response(text, user_id, thread_id) values
+('leave the smoking area then..', 1, 1);
+
+-- display all questions posted and the email of the user who posted them
+select t.text, u.email from fpp_user as u join thread as t on t.user_id = u.id;
+
+-- display all responses to thread with id 1 and the email of the user who responded
+select r.text, u.email from response as r join  fpp_user as u on r.user_id = u.id where r.thread_id = 1;
