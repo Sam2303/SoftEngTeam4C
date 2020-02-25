0c2e4f6e9fa5aeaf12e6c5fb8051980802509d95
thatguywiththatname <simjenner3@gmail.com>
2020-02-25 16:34:03 +0000

    Update SQL file to use better names, update checkUser()

--- src/database.js
+++ src/database.js
@@ -7,31 +7,32 @@ const client = new Client({
     password: 'team4c',
 });
 
-async function connect(){
+async function connect() {
     console.log('Attempting to connect to Database...');
     await client.connect();
     console.log('Connected to Database!');
 }
 
 // Not sure if this is needed? - Simon
-async function disconnect(){
+async function disconnect() {
     await client.end();
 }
 
-async function query(sql){
+async function query(sql) {
     return client.query(sql);
 }
 
-async function checkUser(email, passwordHash) {
-    // returns true if given user name exists and the password hash matches
+// returns true if given user name exists and the password hash matches
+async function checkUser(email, expectedPasswordHash) {
 
-    const {rows} = await query(`select email, password from fpp_user where email = '${email}';`);
+    // This is vulnerable to SQL injection
+    const {rows} = await query(`select password from fpp_user where email = '${email}';`);
 
-    if(rows.length != 0){                   // email exists
-        const [{password}] = rows;
-        return password == passwordHash     // password hash matches?
-    }else{
-        return false                        // email does not exist
+    if (rows.length != 0) { // email exists
+        const [{password_hash}] = rows;
+        return password_hash === expectedPasswordHash // password hash matches?
+    } else {
+        return false // email does not exist
     }
 }
 
--- src/setup.sql
+++ src/setup.sql
@@ -8,40 +8,46 @@ alter user team4c password 'team4c';
 \c team4c;
 
 create table fpp_user(
-    id          serial primary key,
-    email       varchar(50) not null,
-    password    varchar(50) not null
+    id               serial primary key,
+    email            varchar(50) not null,
+    -- Passwords are to be hashed using SHA256
+    -- Just storing the hash like this is not actually great security but its good enough
+    password_hash    char(64) not null
 );
 
-create table thread(
+create table question(
     id          serial primary key,
     text        text not null,
     date        date not null,
     user_id     integer         references fpp_user(id)
 );
 
-create table response(
+create table answer(
     id          serial primary key,
     text        text not null,
     score       integer         default 0,
     user_id     integer         references fpp_user(id),
-    thread_id   integer         references thread(id)
+    question_id   integer         references question(id)
 );
 
--- insert test user
-insert into fpp_user(email, password) values
-('test-email@test.ac.uk', 'ekfrowgnrjwefr');
+-- insert test user (password is "password")
+insert into fpp_user(email, password_hash) values
+(
+    'test-email@test.ac.uk',
+    '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
+);
 
--- insert test thread
-insert into thread(text, date, user_id) values
+-- insert test question
+insert into question(text, date, user_id) values
 ('I need air! I can''t breath!', now(), 1);
 
--- insert test response to thread 
-insert into response(text, user_id, thread_id) values
+-- insert test answer to question 
+insert into answer(text, user_id, question_id) values
 ('leave the smoking area then..', 1, 1);
 
 -- display all questions posted and the email of the user who posted them
-select t.text, u.email from fpp_user as u join thread as t on t.user_id = u.id;
+select t.text, u.email from fpp_user as u join question as t on t.user_id = u.id;
 
--- display all responses to thread with id 1 and the email of the user who responded
-select r.text, u.email from response as r join  fpp_user as u on r.user_id = u.id where r.thread_id = 1;
+-- display all answers to question with id 1 and the email of the user who responded
+select r.text, u.email from answer as r join  fpp_user as u on r.user_id = u.id
+where r.question_id = 1;
