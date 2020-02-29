070b7eeb99e5d441e59591ddd7e5ea5a7387a334
Sam2303 <56475521+Sam2303@users.noreply.github.com>
2020-02-29 19:22:43 +0000

    took the JS out of html and linked the js files

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 
-// window.addEventListener("scroll", function() {
-//     var header = document.querySelector("header");
-//     header.classList.toggle("sticky", window.scrollY > 0);
-// });
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
 
 
 $('.search-button').click(function(){
