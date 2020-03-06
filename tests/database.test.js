259a582e9b620791b2bfc5ad91d0db44e87d4a34
Harry Martin <up896106@myport.ac.uk>
2020-03-06 16:33:29 +0000

    Updated tests to match the return type of functions in database.js

--- tests/database.test.js
+++ tests/database.test.js
@@ -12,14 +12,14 @@ test('Call checkUser with default test user', async () => {
     expect(await db.checkUser(
         'test-email@test.ac.uk',
         '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
-    )).toBe(true);
+    )).toBe(1);
 });
 
 test('Call checkUser with incorrect email', async () => {
     expect(await db.checkUser(
         'incorrect-email@test.ac.uk',
         '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
-    )).toBe(false);
+    )).toBe(-1);
 });
 
 test('Call checkUser with incorrect password', async () => {
@@ -27,5 +27,5 @@ test('Call checkUser with incorrect password', async () => {
         'incorrect-email@test.ac.uk',
         // 6 instead of 5!
         '6e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8',
-    )).toBe(false);
+    )).toBe(-1);
 });
