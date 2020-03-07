64b64c58b8a1d25dc255cc4448b82a43b96225a2
thatguywiththatname <simjenner3@gmail.com>
2020-03-07 23:36:01 +0000

    Format setup.sql using pgFormatter

--- src/setup.sql
+++ src/setup.sql
@@ -1,54 +1,63 @@
-drop database if exists team4c;
-create database team4c;
-drop user if exists team4c;
-create user team4c;
-alter user team4c with superuser;
-alter user team4c password 'team4c';
+DROP DATABASE IF EXISTS team4c;
+CREATE DATABASE team4c;
+
+DROP USER IF EXISTS team4c;
+CREATE USER team4c;
+ALTER USER team4c WITH superuser;
+ALTER USER team4c PASSWORD 'team4c';
 
 \c team4c;
 
-create table fpp_user(
-    id               serial primary key,
-    email            varchar(50) not null,
+CREATE TABLE fpp_user (
+    id serial PRIMARY KEY,
+    email varchar(50) NOT NULL,
     -- Passwords are to be hashed using SHA256
     -- Just storing the hash like this is not actually great security but its good enough
-    password_hash    char(64) not null
+    password_hash char(64) NOT NULL
 );
 
-create table question(
-    id          serial primary key,
-    text        text not null,
-    title       text not null,
-    date        date not null,
-    user_id     integer         references fpp_user(id)
+CREATE TABLE question (
+    id serial PRIMARY KEY,
+    text text NOT NULL,
+    title text NOT NULL,
+    date date NOT NULL,
+    user_id integer REFERENCES fpp_user (id)
 );
 
-create table answer(
-    id          serial primary key,
-    text        text not null,
-    score       integer         default 0,
-    user_id     integer         references fpp_user(id),
-    question_id   integer         references question(id)
+CREATE TABLE answer (
+    id serial PRIMARY KEY,
+    text text NOT NULL,
+    score integer DEFAULT 0,
+    user_id integer REFERENCES fpp_user (id),
+    question_id integer REFERENCES question (id)
 );
 
 -- insert test user (password is "password")
-insert into fpp_user(email, password_hash) values
-(
-    'test-email@test.ac.uk',
-    '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'
-);
+INSERT INTO fpp_user (email, password_hash)
+    VALUES ('test-email@test.ac.uk', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
 
 -- insert test question
-insert into question(text, title, date, user_id) values
-('I need air! I can''t breath!', 'This is a titleee', now(), 1);
+INSERT INTO question (text, title, date, user_id)
+    VALUES ('I need air! I can''t breath!', 'This is a titleee', now(), 1);
 
 -- insert test answer to question
-insert into answer(text, user_id, question_id) values
-('leave the smoking area then..', 1, 1);
+INSERT INTO answer (text, user_id, question_id)
+    VALUES ('leave the smoking area then..', 1, 1);
 
 -- display all questions posted and the email of the user who posted them
-select t.text, u.email from fpp_user as u join question as t on t.user_id = u.id;
+SELECT
+    t.text,
+    u.email
+FROM
+    fpp_user AS u
+    JOIN question AS t ON t.user_id = u.id;
 
 -- display all answers to question with id 1 and the email of the user who responded
-select r.text, u.email from answer as r join  fpp_user as u on r.user_id = u.id
-where r.question_id = 1;
+SELECT
+    r.text,
+    u.email
+FROM
+    answer AS r
+    JOIN fpp_user AS u ON r.user_id = u.id
+WHERE
+    r.question_id = 1;
