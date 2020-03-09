26098c98d689b3cfc56cde6df41fc071018c46fd
Harry Martin <up896106@myport.ac.uk>
2020-03-09 17:57:40 +0000

    Add example.js file which contains an example of how to add event listeners and how to interface with our api, Add example.js script tag in index.html, Remove href from markup (added to js)

---
+++ src/static/js/example.js
@@ -0,0 +1,37 @@
+// elements we wish to add the event listener to.
+const elements = document.getElementsByClassName('login-button');
+
+// for each element we attach the event listener.
+Object.values(elements).forEach((element) => {
+    // in this case we're using the 'click' event
+    element.addEventListener('click', async () => {
+        // The API route
+        const url = '/api/auth/login';
+
+        // The data we want to send
+        // Credentials taken from SQL setup file
+        const data = {
+            email: 'test-email@test.ac.uk',
+            password: 'password',
+        };
+
+        // Fetch returns a response object that tells us different things about the response
+        const response = await fetch(url, {
+            method: 'POST', // Send a POST request
+            headers: { 'Content-Type': 'application/json' }, // Tell the server we are sending JSON
+            body: JSON.stringify(data), // Data type must match "Content-Type" header
+        });
+
+        // Get the JSON data from the response
+        const returned = await response.json();
+
+        // Check if it worked
+        if (returned.success === true) {
+            // redirect to homepage.html
+            window.location.href = 'homepage.html';
+            console.log("You're logged in!");
+        } else {
+            console.log('Failed to log in!');
+        }
+    });
+});
