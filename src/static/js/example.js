88904d1783f7d7015bd86e120e4756bd70eee57f
Sam2303 <56475521+Sam2303@users.noreply.github.com>
2020-03-10 17:04:56 +0000

    made the register page keep with theme and added JS for registering an account

--- src/static/js/example.js
+++ src/static/js/example.js
@@ -10,9 +10,12 @@ Object.values(elements).forEach((element) => {
 
         // The data we want to send
         // Credentials taken from SQL setup file
+        // making variables to ge† the values in the email and password input boxes
+        let email = document.body.getElementById('email').value;
+        let password = document.body.getElementById('password').value;
         const data = {
-            email: 'test-email@test.ac.uk',
-            password: 'password',
+            email: email,
+            password: password,
         };
 
         // Fetch returns a response object that tells us different things about the response
@@ -32,6 +35,43 @@ Object.values(elements).forEach((element) => {
             console.log("You're logged in!");
         } else {
             console.log('Failed to log in!');
+        }
+    });
+});
+
+// elements we wish to add the event listener to.
+const registerElements = document.getElementsByClassName('register-button2');
+Object.values(registerElements).forEach((registerElements) => {
+    // in this case we're using the 'click' event
+    element.addEventListener('click', async () => {
+        // The API route
+        const url = '/api/auth/register';
+
+        // The data we want to send
+        // Credentials taken from SQL setup file
+        // making variables to ge† the values in the email and password input boxes
+        let reg-email = document.body.getElementById('reg-email').value;
+        let reg-password = document.body.getElementById('reg-password').value;
+        const data = {
+            email: reg-email,
+            password: reg-password,
+        };
+
+        const reg-response = await fetch(url, {
+            method: 'POST', // Send a POST request
+            headers: { 'Content-Type': 'application/json' }, // Tell the server we are sending JSON
+            body: JSON.stringify(data), // Data type must match "Content-Type" header
+        });
+        // Get the JSON data from the response
+        const returned = await reg-esponse.json();
+
+        // Check if it worked
+        if (returned.success === true) {
+            // redirect to homepage.html
+            window.location.href = 'homepage.html';
+            console.log("You're registered");
+        } else {
+            console.log('Failed to register');
         }
     });
 });
