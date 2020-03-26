59115976d39f16e67c872eedd2e5214bfeec344a
thatguywiththatname <simjenner3@gmail.com>
2020-03-26 14:45:15 +0000

    Update Javascript

--- src/database.js
+++ src/database.js
@@ -3,6 +3,7 @@
  */
 
 const { Client } = require('pg');
+const utils = require('./utils');
 
 // When writing postgres queries, use http://sqlformat.darold.net/ for readability.
 
@@ -215,14 +216,13 @@ async function insertAnswer(userId, questionId, text) {
  */
 async function voteOnAnswer(answerId, upvote) {
     const voteDifference = upvote ? 1 : -1;
-    console.log("voteDifference: ", voteDifference);
 
     const { rows } = await query(`
     UPDATE answer
     SET score = (SELECT score FROM answer WHERE id = ${answerId}) + ${voteDifference}
     RETURNING score;
     `);
-    console.log(rows);
+
     return Object.values(rows[0])[0];
 }
 
@@ -233,8 +233,7 @@ async function voteOnAnswer(answerId, upvote) {
  */
 async function searchQuestions(searchText) {
     // Textual search
-    console.log(searchText);
-    if (searchText !== '') {
+    if (utils.isTextString(searchText)) {
         const { rows } = await query(`
         SELECT x.id, x.title FROM
         (
@@ -243,10 +242,9 @@ async function searchQuestions(searchText) {
             ORDER BY similarity DESC
         ) as x;
         `);
-        console.log("Not Blank");
-        console.log(rows);
         return rows;
     }
+
     // searchText is empty string, search all by date.
     // order by date without selecting date
     const { rows } = await query(`
@@ -257,8 +255,6 @@ async function searchQuestions(searchText) {
         ORDER BY date DESC
     ) as x;
     `);
-    console.log("Blank");
-    console.log(rows);
     return rows;
 }
 
--- src/static/js/register.js
+++ src/static/js/register.js
@@ -1,13 +1,6 @@
-// When scrolling nav bar sticks to top
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0);
-});
-
-// elements we wish to add the event listener to.
 const registerElements = document.getElementsByClassName('reg-button');
-Object.values(registerElements).forEach((registerElements) => {
-    // in this case we're using the 'click' event
+
+Object.values(registerElements).forEach((element) => {
     element.addEventListener('click', async () => {
         // The API route
         const url = '/api/auth/register';
@@ -15,8 +8,8 @@ Object.values(registerElements).forEach((registerElements) => {
         // The data we want to send
         // Credentials taken from SQL setup file
         // making variables to ge† the values in the email and password input boxes
-        let reg_email = document.body.getElementById('reg-email').value;
-        let reg_password = document.body.getElementById('reg-password').value;
+        const reg_email = document.body.getElementById('reg-email').value;
+        const reg_password = document.body.getElementById('reg-password').value;
         const data = {
             email: reg_email,
             password: reg_password,
